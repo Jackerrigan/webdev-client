22dcb9721fb3090f2dc71b5f38ca09816bd1b2d4
Jackerrigan <jack.kerrigan05@gmail.com>
2026-09-25 11:27:07 -0400

    TOC self/ai

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -1,3 +1,35 @@
+import Link from "next/link";
+
 export default function TOC() {
-  return null;
+  return (
+    <nav aria-label="Labs table of contents">
+      <h3>Table of Contents</h3>
+      <ul>
+        <li>
+          <Link href="/labs">Home</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab1">Lab 1</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab2">Lab 2</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab3">Lab 3</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab4">Lab 4</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab5">Lab 5</Link>
+        </li>
+        <li>
+          <Link href="/book/ch1" id="wd-toc-book-link">
+            Chapter 1
+          </Link>
+        </li>
+      </ul>
+      <h6>so many weapons, the deadliest is your faith</h6>
+    </nav>
+  );
 }
