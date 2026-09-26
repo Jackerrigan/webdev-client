f221ca4d60952c82036e09d70674b78d3b11f1e5
Jackerrigan <jack.kerrigan05@gmail.com>
2026-09-26 00:03:49 -0400

    1.4.6

---
+++ app/(kambaz)/courses/[cid]/assignments/AssignmentItem.tsx
@@ -0,0 +1,18 @@
+export default function AssignmentItem({
+  cid,
+  aid,
+  title,
+  details,
+}: {
+  cid: string;
+  aid: string;
+  title: string;
+  details: string;
+}) {
+  return (
+    <li className="wd-assignment-list-item">
+      {/* Link the title to /courses/${cid}/assignments/${aid}
+          (className wd-assignment-link), then show details underneath */}
+    </li>
+  );
+}
